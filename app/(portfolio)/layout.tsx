33349ead510d1bd108520ab9cf5785254d95ba2b
MahdiT54 <mahditanzim2@gmail.com>
2026-02-21 11:35:00 -0500

    AgentKit & Shadcn & Aceternity elemented inputted

--- app/(portfolio)/layout.tsx
+++ app/(portfolio)/layout.tsx
@@ -4,6 +4,7 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { SanityLive } from "@/sanity/lib/live";
 import SidebarToggle from "@/components/SidebarToggle";
+import { ThemeProvider } from "@/components/theme-provider";
 import Script from "next/script";
 
 export default function RootLayout({
@@ -15,26 +16,33 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body suppressHydrationWarning>
-          <Script
-            src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
-            strategy="afterInteractive"
-          />
-          <SidebarProvider>
-            <SidebarInset>{children}</SidebarInset>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Script
+              src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
+              strategy="afterInteractive"
+            />
+            <SidebarProvider defaultOpen={false}>
+              <SidebarInset>{children}</SidebarInset>
 
-            <AppSidebar side="right" />
+              <AppSidebar side="right" />
 
-            {/* <FloatingDock /> */}
-            <SidebarToggle />
+              {/* <FloatingDock /> */}
+              <SidebarToggle />
 
-            {/* Mode Toggle - Desktop: bottom right next to AI chat, Mobile: top right next to burger menu */}
-            {/* <div className="fixed md:bottom-6 md:right-24 top-4 right-18 md:top-auto md:left-auto z-20">
+              {/* Mode Toggle - Desktop: bottom right next to AI chat, Mobile: top right next to burger menu */}
+              {/* <div className="fixed md:bottom-6 md:right-24 top-4 right-18 md:top-auto md:left-auto z-20">
                 <div className="w-10 h-10 md:w-12 md:h-12">
                   <ModeToggle />
                 </div>
               </div> */}
-          </SidebarProvider>
-          <SanityLive />
+            </SidebarProvider>
+            <SanityLive />
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
